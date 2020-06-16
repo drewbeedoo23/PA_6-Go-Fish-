e28809cfd8e5fbc9ee4ce33550c697d8ddf93118
tobias.russell <tobias.russell@wsu.edu>
2020-06-15 18:57:56 -0700

    removed redundent code

--- main.cpp
+++ main.cpp
@@ -6,11 +6,9 @@ int main(){
     string input;
     int hold=0;
     collected p1coll, p2coll;
-    sf::Texture felt, back;
+    sf::Texture felt;
     srand(time(nullptr));
-    back.setRepeated(false);
     felt.loadFromFile("felttable.png");
-    back.loadFromFile("gray_back.png");
     Deck.initializefromfile();
     Deck.shuffle();
     for(int i=0;i<7;++i){
@@ -19,15 +17,8 @@ int main(){
     } 
     sf::RenderWindow window(sf::VideoMode(1366,768),"Go Fish!");
     sf::RectangleShape board(sf::Vector2f(1366.f,768.f));
-    sf::Sprite deckback(back);
     board.setTexture(&felt);
-    deckback.setPosition(100,342);
-    deckback.setScale(0.078148f,0.080492f);
     board.setPosition(0,0);
-    //p1.draw(1,window);
-    //window.draw(board);
-    //Deck.draw(window);
-    //window.display();
     while(window.isOpen()){
         window.draw(board);
         playerturn(p1,p2,p1coll,Deck,window,1);
